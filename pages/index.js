6dd3a07a71206dd42e2ba0dbede442d5f980ac87
ChristianLux <christian.lux@sparepartsnow.de>
2021-12-01 15:52:14 +0100

    feat: adds a/b testing package

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from "next/head";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,12 +11,16 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Read{' '}
+          Read{" "}
           <Link href="/posts/first-post">
             <a>this page!</a>
           </Link>
         </h1>
-
+        <h2>
+          <Link href="/abtests/abtest">
+            <a>A/B Test</a>
+          </Link>
+        </h2>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
@@ -58,8 +62,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className="logo" />
+          Powered by <img src="/vercel.svg" alt="Vercel" className="logo" />
         </a>
       </footer>
 
@@ -209,5 +212,5 @@ export default function Home() {
         }
       `}</style>
     </div>
-  )
+  );
 }
